be9c0fff41baf9c134525eea88d43966b28107dd
Ilari Mäkelä <ilari.makela@mearra.com>
2012-02-09 19:11:51 +0200

    Importing codebase from sandbox project.

--- README.txt
+++ README.txt
@@ -1,11 +1,34 @@
+This module integrates the great HTML5 front-end editor Aloha Editor to Drupal.
+With Aloha Editor you can quickly edit the content on the front-end without the
+need to go to edit section of node or block.
+
+REQUIREMENTS
+------------------
+-Libraries module (http://drupal.org/project/libraries)
+-Aloha Editor library (http://aloha-editor.org/)
+
 INSTALL
+------------------
+-Download the aloha editor from http://www.aloha-editor.org/ and extact it
+ into 'sites/all/libraries/aloha'.
+-Download and install the Libraries module
+-Install this module
 
-Download the aloha editor from http://www.aloha-editor.org/ 
+NOTICE
+------------------
+Because Aloha Editor is also under active development some of the plugins might
+not work. So if you face a problem where the editor toolbar is not showing up
+you just have to try to found out which of the plugins is causing the problem.
 
-Then extact into the path 'sites/all/libraries/aloha'.
+The aloha library includes a copy of ExtJS library. This version of the ExtJS
+library overrides the Array prototype which may cause obscure bugs with existing
+javascript in Drupal or Drupal modules. If you do experience unexplainable
+javascript errors after enabling the module, try disabling the module to see if
+the error is resolved and report the issue here if so.
 
-Now the module will use the libary.
+The aloha library is in the process of being upgraded to use jQuery UI instead
+of ExtJS so this should not be a problem with future versions of the library.
+https://github.com/alohaeditor/Aloha-Editor/tree/dev-jqueryui
 
-Go any node and click or highlight the body text and edit from there.
-
-It saves when you click away from the text as well as change the formating.
+This is a forked copy of original project from drupal.org because the maintainers
+are inactive at the moment.
--- aloha.js
+++ aloha.js
@@ -1,53 +1,85 @@
+/**
+ * @file
+ * Javascript to attach Aloha Editor to the right fields and blocks.
+ */
+
+(function(window, undefined) {
+  var Aloha = window.Aloha || (window.Aloha = {});
+
+  // Set settings
+  Aloha.settings = settings.aloha.alohaSettings;
+});
+
 (function($) {
   Drupal.behaviors.alohaEditor = {
-    attach: function (context, settings) {
+    attach: function(context, settings) {
+      Drupal.behaviors.alohaEditor.fixExtJsArrayPrototypeOverride();
       // Run only if aloha is required
       if (settings.aloha) {
-        // Aloha settings
-        GENTICS.Aloha.settings = settings.aloha.alohaSettings;
-
-        for (nid in settings.aloha.nodes) {
-          // Retrieving container
-          var container = $('#aloha-container-' + nid);
-
-          // Setting up Aloha and events associated
-          container.aloha();
-          container.blur(function() {
-            var html = $(this).html();
-            if (settings.aloha.nodes[nid].html != html) {
-              Drupal.behaviors.alohaEditor.save(nid, html);
-              // Update HTML
-              settings.aloha.nodes[nid].html = html;
-            }
-            $(this).removeClass('aloha-edit');
-          });
-          container.focus(function() {
-            $(this).addClass('aloha-edit');
-          });
-
-          // Store HTML
-          settings.aloha.nodes[nid].html = container.html();
-
-          // Setting up exit event
-          $(window).unload(function() {
-            var html = $('#aloha-container-' + nid).html();
-            if (settings.aloha.nodes[nid].html != html) {
-              Drupal.behaviors.alohaEditor.save(nid, html);
+        Aloha.ready(function() {
+          Aloha.require(['aloha', 'aloha/jquery'], function(Aloha, $) {
+            // Load Aloha for each editable content region
+            for (var key in settings.aloha.regions) {
+              Drupal.behaviors.alohaEditor.attachRegion(key, settings.aloha.regions[key]);
             }
           });
-        }
+        });
       }
     },
-    save: function(nid, html) {
-      // Save node
+
+    attachRegion: function(key, region) {
+      // Get the container
+      var $container = Aloha.jQuery('#aloha-target-' + key).parent();
+      var saveIfChanged = function () {
+        var html = Drupal.behaviors.alohaEditor.getContentFromContainer($container);
+        if (region.html != html) {
+          region.html = html;
+          Drupal.behaviors.alohaEditor.save(key, region);
+        }
+      };
+      // Store the original HTML to test if we need to fire save
+      region.html = Drupal.behaviors.alohaEditor.getContentFromContainer($container);
+      // Set up Aloha and associated events
+      $container.aloha();
+      // Save if required when leaving the edit area
+      $container.blur(saveIfChanged);
+      // Save if required when window is closed or we navigate to a new page
+      $(window).unload(saveIfChanged);
+    },
+
+    // Strip our span target and any following tags from content to be saved.
+    getContentFromContainer: function(container) {
+      var $containerClone = jQuery(container).clone();
+      $containerClone.find('.aloha-target, .aloha-target ~ *').remove();
+      return $containerClone.html();
+    },
+
+    // Workaround for http://drupal.org/node/1404584
+    fixExtJsArrayPrototypeOverride:function () {
+      if (Array.prototype.remove) {
+        delete Array.prototype.remove;
+        Ext.applyIf(Array.prototype, {
+          remove:function (o) {
+            if (!this.indexOf) return this;
+            var index = this.indexOf(o);
+            if (index != -1) {
+              this.splice(index, 1);
+            }
+            return this;
+          }
+        });
+      }
+    },
+
+    save: function(key, region) {
       $.ajax({
         type: "POST",
-        url: Drupal.settings.basePath + 'node/' + nid + '/aloha/save',
-        data: {body: html, lang: Drupal.settings.aloha.nodes[nid].lang},
+        url: Drupal.settings.basePath + 'aloha/' + region.type + '/save',
+        data: region,
         success: function(obj) {
           if (obj.status == 'saved') {
             var element = '<div class="aloha-status">' + Drupal.t('%title has been saved.', {'%title': obj.title}) + '</div>';
-            $(element).insertBefore($('#aloha-container-' + obj.nid)).delay(1300).fadeOut(function () {
+            $(element).insertBefore($('#aloha-target-' + key).parent()).delay(1300).fadeOut(function () {
               $(this).remove();
             });
           }
