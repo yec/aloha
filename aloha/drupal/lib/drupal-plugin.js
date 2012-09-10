022bf2daaabdc63182e4913f445fd09029140d2e
nod_ <nod_@598310.no-reply.drupal.org>
2012-09-10 13:59:08 +0200

    Issue #1778750 by nod_: Clean-up: aloha/drupal/*.

--- aloha/drupal/lib/drupal-plugin.js
+++ aloha/drupal/lib/drupal-plugin.js
@@ -1,17 +1,11 @@
-define( [
+define([
   'aloha',
   'aloha/plugin',
   'jquery',
   'aloha/contenthandlermanager',
   'drupal/drupalcontenthandler',
-  'drupal/repository',
-], function (
-  Aloha,
-  Plugin,
-  $,
-  ContentHandlerManager,
-  DrupalContentHandler
-) {
+  'drupal/repository'
+], function (Aloha, Plugin, $, ContentHandlerManager, DrupalContentHandler) {
   'use strict';
 
   var pluginNamespace = 'aloha-drupal';
@@ -20,91 +14,91 @@ define( [
     /**
      * Configure the available languages.
      */
-    languages: [ 'en' ],
+    languages: ['en'],
 
     /**
      * Default configuration.
      */
-    config: [ ],
+    config: [],
 
     /**
      * Initialize the plug-in.
      */
     init: function () {
-      var elementMapping,
-          allowedTags;
 
-      elementMapping = {
-          'a': 'link',
-          'b': 'bold',
-          'strong': 'strong',
-          'i': 'italic',
-          'em': 'emphasis',
-          'del': 'strikethrough',
-          'sub': 'subscript',
-          'sup': 'superscript',
-          'u': 'underline',
-          's': 'strikethrough2',
-          'ol': 'orderedlist',
-          'ul': 'unorderedlist',
-          'p': 'p',
-          'pre': 'pre',
-          'h1': 'h1',
-          'h2': 'h2',
-          'h3': 'h3',
-          'h4': 'h4',
-          'h5': 'h5',
-          'h6': 'h6',
+      var allowedTags;
+
+      var elementMapping = {
+        'a': 'link',
+        'b': 'bold',
+        'strong': 'strong',
+        'i': 'italic',
+        'em': 'emphasis',
+        'del': 'strikethrough',
+        'sub': 'subscript',
+        'sup': 'superscript',
+        'u': 'underline',
+        's': 'strikethrough2',
+        'ol': 'orderedlist',
+        'ul': 'unorderedlist',
+        'p': 'p',
+        'pre': 'pre',
+        'h1': 'h1',
+        'h2': 'h2',
+        'h3': 'h3',
+        'h4': 'h4',
+        'h5': 'h5',
+        'h6': 'h6'
       };
 
       // Register DrupalContentHandler.
       ContentHandlerManager.register('drupal', DrupalContentHandler);
 
-      Aloha.bind('aloha-editable-activated', function($event, params) {
+      Aloha.bind('aloha-editable-activated', function ($event, params) {
         var allowedTagsList = Aloha.activeEditable.originalObj
-                              .closest('[data-allowed-tags]')
-                              .data('allowed-tags');
+          .closest('[data-allowed-tags]')
+          .data('allowed-tags');
 
         if (allowedTagsList) {
           allowedTags = allowedTagsList.split(',');
 
-          $.each(elementMapping, function(element, className) {
-            if ($.inArray(element, allowedTags) == -1) {
-              $('span.ui-button-icon-primary[data-html-tag="' + className + '"]')
-              .closest('button')
-              .addClass('aloha-drupal-ui-state-hidden');
+          for (var element in elementMapping) {
+            if (elementMapping.hasOwnProperty(element) && $.inArray(element, allowedTags) === -1) {
+              $('span.ui-button-icon-primary[data-html-tag="' + elementMapping[element] + '"]')
+                .closest('button')
+                .addClass('aloha-drupal-ui-state-hidden');
             }
-          });
+          }
         }
 
         // Clean up empty component groups.
-        $.each($('.aloha-ui-component-group'), function(){
+        $('.aloha-ui-component-group').each(function () {
+          var $this = $(this);
           // Check for hidden nodes and empty nodes.
           var cc = 0;
-          $.each($(this).children(), function() {
-            if ($(this).css('display') == 'none' || !$(this).text()) {
+          $this.children().each(function () {
+            var $that = $(this);
+            if ($that.css('display') === 'none' || !$that.text()) {
               cc++;
             }
           });
-          if (cc == jQuery(this).children().length) {
-            jQuery(this).hide();
+          if (cc === $this.children().length) {
+            $this.hide();
           }
         });
       });
 
-      Aloha.bind('aloha-editable-deactivated', function($event, params) {
-        $.each(elementMapping, function(element, className) {
-          if ($.inArray(element, allowedTags) == -1) {
-            $('span.ui-button-icon-primary[data-html-tag="' + className + '"]')
-            .closest('button')
-            .removeClass('aloha-drupal-ui-state-hidden');
+      Aloha.bind('aloha-editable-deactivated', function ($event, params) {
+        for (var element in elementMapping) {
+          if (elementMapping.hasOwnProperty(element) && $.inArray(element, allowedTags) === -1) {
+            $('span.ui-button-icon-primary[data-html-tag="' + elementMapping[element] + '"]')
+              .closest('button')
+              .removeClass('aloha-drupal-ui-state-hidden');
           }
-        });
+        }
 
         // Restore previously empty component groups.
-        $.each($('.aloha-ui-component-group'), function() {
-            $(this).show();
-        });
+        $('.aloha-ui-component-group').show();
       });
     }
   });
--- aloha/drupal/lib/drupalcontenthandler.js
+++ aloha/drupal/lib/drupalcontenthandler.js
@@ -1,92 +1,85 @@
 define([
-    'aloha/core',
-    'jquery',
-    'aloha/contenthandlermanager',
-    'aloha/console',
-    'vendor/sanitize'
-    ],
-function( Aloha,
-            jQuery,
-            ContentHandlerManager,
-            console,
-            Sanitize ) {
-	"use strict";
+  'aloha/core',
+  'jquery',
+  'aloha/contenthandlermanager',
+  'aloha/console',
+  'vendor/sanitize'
+],
+  function (Aloha, $, ContentHandlerManager, console, Sanitize) {
+    "use strict";
 
-	var sanitize, setting = {};
+    var sanitize, setting = {};
 
-	// very restricted sanitize config
-	setting.restricted = {
-		elements: [ 'b', 'em', 'i', 'strong', 'u', 'del', 'p', 'span', 'div', 'br' ]
-	}
+    // very restricted sanitize config
+    setting.restricted = {
+      elements: ['b', 'em', 'i', 'strong', 'u', 'del', 'p', 'span', 'div', 'br']
+    };
 
-	// basic sanitize config
-	setting.basic = {
-		elements: [
-			'a', 'abbr', 'b', 'blockquote', 'br', 'cite', 'code', 'dd', 'del', 'dl', 'dt', 'em',
-			'i', 'li', 'ol', 'p', 'pre', 'q', 'small', 'strike', 'strong', 'sub',
-			'sup', 'u', 'ul' ],
+    // basic sanitize config
+    setting.basic = {
+      elements: [
+        'a', 'abbr', 'b', 'blockquote', 'br', 'cite', 'code', 'dd', 'del', 'dl',
+        'dt', 'em',
+        'i', 'li', 'ol', 'p', 'pre', 'q', 'small', 'strike', 'strong', 'sub',
+        'sup', 'u', 'ul'
+      ],
 
-		attributes: {
-			'a' : ['href'],
-			'blockquote' : ['cite'],
-			'q' : ['cite'],
-			'abbr': ['title']
-		},
+      attributes: {
+        'a': ['href'],
+        'blockquote': ['cite'],
+        'q': ['cite'],
+        'abbr': ['title']
+      },
 
-		protocols: {
-			'a' : {'href': ['ftp', 'http', 'https', 'mailto', '__relative__']},
-			'blockquote' : {'cite': ['http', 'https', '__relative__']},
-			'q' : {'cite': ['http', 'https', '__relative__']}
-		}
-	}
+      protocols: {
+        'a': {'href': ['ftp', 'http', 'https', 'mailto', '__relative__']},
+        'blockquote': {'cite': ['http', 'https', '__relative__']},
+        'q': {'cite': ['http', 'https', '__relative__']}
+      }
+    };
 
-	function initSanitize () {
-		var config;
+    var DrupalContentHandler = ContentHandlerManager.createHandler({
+      /**
+       * Handle the content from eg. paste action and sanitize the html
+       * @param content
+       */
+      handleContent: function (content) {
+        var config;
+        // sanitize does not work in IE7. It tries to set the style attribute via setAttributeNode() and this is know to not work in IE7
+        // (see http://www.it-blogger.com/2007-06-22/microsofts-internetexplorer-und-mitglied-nicht-gefunden/ as a reference)
+        if ($.browser.msie && $.browser.version <= 7) {
+          return content;
+        }
 
-        config = this.setting.basic;
+        // dynamic allowed tags drupal sprint
+        var allowedTags = Aloha.activeEditable.originalObj
+          .closest('[data-allowed-tags]')
+          .data('allowed-tags');
+        if (allowedTags) {
+          var allows = allowedTags.split(',');
+          sanitize = new Sanitize({elements: allows});
+        }
+        else if (typeof sanitize === 'undefined') {
+          config = this.setting.basic;
+          // add a filter to stop cleaning elements with contentEditable "false"
+          config.filters = [
+            function (elem) {
+              return elem.contentEditable !== "false";
+            }
+          ];
+          sanitize = new Sanitize(config);
+        }
 
-		// add a filter to stop cleaning elements with contentEditable "false"
-		config.filters = [function( elem ) {
-			return elem.contentEditable != "false";
-		}];
-		sanitize = new Sanitize( config );
-	}
+        if (typeof content === 'string') {
+          content = $('<div>' + content + '</div>').get(0);
+        }
+        else if (content instanceof $) {
+          content = $('<div>').append(content).get(0);
+        }
 
-	var DrupalContentHandler = ContentHandlerManager.createHandler({
-		/**
-		 * Handle the content from eg. paste action and sanitize the html
-		 * @param content
-		 */
-		handleContent: function( content )  {
-			// sanitize does not work in IE7. It tries to set the style attribute via setAttributeNode() and this is know to not work in IE7
-			// (see http://www.it-blogger.com/2007-06-22/microsofts-internetexplorer-und-mitglied-nicht-gefunden/ as a reference)
-			if (jQuery.browser.msie && jQuery.browser.version <= 7) {
-				return content;
-			}
+        return $('<div>').append(sanitize.clean_node(content)).html();
+      }
+    });
 
-			// dynamic allowed tags drupal sprint
-			var allowedTags = Aloha.activeEditable.originalObj
-				.closest('[data-allowed-tags]')
-				.data('allowed-tags');
-			if ( allowedTags ) {
-			    var allows = allowedTags.split(',');
-			    var config = {};
-			    config.elements = allows;
-
-		        sanitize = new Sanitize( config );
-			} else if ( typeof sanitize === 'undefined' ) {
-			   initSanitize();
-			}
-
-			if ( typeof content === 'string' ){
-				content = jQuery( '<div>' + content + '</div>' ).get(0);
-			} else if ( content instanceof jQuery ) {
-				content = jQuery( '<div>' ).append(content).get(0);
-			}
-
-			return jQuery('<div>').append(sanitize.clean_node(content)).html();
-		}
-	});
-
-	return DrupalContentHandler;
-});
+    return DrupalContentHandler;
+  });
